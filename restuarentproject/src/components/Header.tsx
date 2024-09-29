51b2c41fdbfc46f1af7726f83a0db7df8faa0afe
salmanfarooqi <salmanfarooqi1272001@gmail.com>
2024-09-28 21:33:16 -0700

    next-setup

---
+++ restuarentproject/src/components/Header.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='bg-gray-100 p-4 w-full'>
+    
+    {/* left section */}
+       <div>
+           food website
+        </div>  
+
+    {/* center section */}
+    <div>
+        <ul>
+            <li>Home</li>
+            <li>Menu</li>
+            <li>Offers</li>
+        </ul>
+    </div>
+    </div>
+  )
+}
+
+export default Header
